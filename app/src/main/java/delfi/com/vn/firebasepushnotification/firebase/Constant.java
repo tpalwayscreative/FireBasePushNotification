cc40a9807b2f93a9080f0f851fa35d1e0695c98e
Mr Thanh Phong <tranthanhphongcntt2009@gmail.com>
2017-08-29 17:33:40 +0700

    Initial commit

---
+++ app/src/main/java/delfi/com/vn/firebasepushnotification/firebase/Constant.java
@@ -0,0 +1,10 @@
+package delfi.com.vn.firebasepushnotification.firebase;
+
+/**
+ * Created by PC on 8/29/2017.
+ */
+
+public class Constant {
+
+    //http://tpalwayscreative.esy.es/FileUpload/push_notification.php?id=fgaJuYBH57w:APA91bGQyTprMG-JcTsbd92p03NCjbycM5mLefHEh_bT2Z7QQFwG7Jzrc0F_btbHX2Ct6oBoSyeGv1NZvNZnAP2qksxusTT5d8CDW3USegYMFVT4CqvUL2F3SW_LmZ99AiDbAMJAULGQ
+}
